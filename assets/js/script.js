32d881dc65a46cdbd8a4b67a4a9c499b1b1a66af
AliBaker <alibakerconsulting@gmailcom>
2022-03-29 22:07:37 -0400

    Pretty much functional. Just need to implement the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,86 @@
 var displayTimeEl = $('#displayTime');
 var tbodyEl = $('#tableBody');
 var submitBtn = $('submit-btn');
-var newProjectBtn=$('new-project-btn')
+var newProjectBtn=$('#new-project-btn')
 var nameInputEl = $('#project-name');
-var typeInputEl = $('project-type');
-var hourlyInputEl = $('hourly-wage');
+var typeInputEl = $('#project-type');
+var wageInputEl = $('#hourly-wage');
 var dateInputEl = $('#due-date');
 var skillsListEl = $('#skills-list');
+var formEl= $('#newProjectForm')
 
-var printSkills = function (name, date) {
-  var listEl = $('<li>');
-  var listDetail = name.concat(' on ', date);
-  listEl.addClass('list-group-item').text(listDetail);
-  listEl.appendTo(skillsListEl);
+var currentTime= moment().format("MMM Do, YYYY hh:mm:ss");
+
+function projectObj(name, type, wage, due,) {
+  this.name = name;
+  this.type = type;
+  this.wage = wage;
+  this.due = due;
+}
+
+
+var printProject = function () {
+  var dueDay=moment(currentProject.due).dayOfYear();
+  var todayOfYear=moment().dayOfYear();
+  var daysLeft=dueDay-todayOfYear;
+  var estimate=currentProject.wage * 24 * daysLeft;
+  var wholeRow="<tr><td>" + currentProject.name + "</td><td>"+ currentProject.type+ "</td><td>"+ currentTime+ "</td><td>"+ currentProject.wage+ "</td><td>"+ currentProject.due+ "</td><td>"+ daysLeft+ "</td><td>"+ estimate+ "</td><td>"+ '<button type="button" class="btn btn-danger">Delete</button>'+ "</td></tr>";
+  // listEl.addClass('list-group-item').text(listDetail);
+  tbodyEl.append(wholeRow);
 };
 
-var handleFormSubmit = function (event) {
+
+
+var handleFormSubmit = function () {
+  
+  currentProject= new projectObj("","",0,"");
+  currentProject["name"]=nameInputEl.val();
+  currentProject["type"]=typeInputEl.val();
+  currentProject["wage"]=wageInputEl.val();
+  currentProject["due"]=dateInputEl.val();
+  console.log(currentProject);
+  printProject();
+
+};
+
+formEl.on('submit', function(event) {
   event.preventDefault();
+  console.log("Submit Clicked");
+  handleFormSubmit();
+  $('#newProjectModal').modal('hide');
+  return false;
+});
 
-  var nameInput = nameInputEl.val();
-  var dateInput = dateInputEl.val();
-
-  if (!nameInput || !dateInput) {
-    console.log('You need to fill out the form!');
-    return;
+tbodyEl.on("click", function (evt) {
+  console.log("Click Registered");
+  var target = evt.target;
+  console.log(target);
+  console.log($(target).parents("tr"));
+  if (target.matches("button")) {
+    console.log("Condition met");
+    // $(target).parents("tr").remove($(target).parents("tr"));
+    $(target).closest('tr').remove();
   }
+});
 
-  printSkills(nameInput, dateInput);
+newProjectBtn.on("click", function(){
+  console.log("Click Event New Project")
+  nameInputEl.textcontent="";
+  typeInputEl.textcontent="";
+})
 
-  nameInputEl.val('');
-  dateInputEl.val('');
-};
+function countdown() {
+  timeInterval = setInterval(function () {
 
-// formEl.on('submit', handleFormSubmit);
-var currentTime= moment();
-displayTimeEl.text(currentTime.format("MMM Do, YYYY hh:mm:ss"));
+  }, 1000);
+}
 
-// Datepicker widget
+displayTimeEl.text(currentTime);
+
+//Datepicker widget
 $(function () {
   $('#due-date').datepicker({
     changeMonth: true,
     changeYear: true,
   });
 });
-
-// Add interaction here
-$( function() {
-  $( "#skills-list" ).sortable({
-    revert: true
-  });
-  $( "#draggable" ).draggable({
-    connectToSortable: "#skills-list",
-    helper: "clone",
-    revert: "invalid"
-  });
-  $( "ul, li" ).disableSelection();
-} );
